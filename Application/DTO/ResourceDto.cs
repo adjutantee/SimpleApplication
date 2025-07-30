6bf33db534f5b8a708b4721cd6112faf2156d637
adjutantee <izagakhmaevra@gmail.com>
2025-07-30 16:19:47 +0300

    Добавление контекста

---
+++ Application/DTO/ResourceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTO
+{
+    public class ResourceDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string State { get; set; } = "Active";
+    }
+}
