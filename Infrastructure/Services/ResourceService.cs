68477377c2f7eb05e1faf75d16419632b5f9b0c7
adjutantee <izagakhmaevra@gmail.com>
2025-07-30 16:54:58 +0300

    Добавление интерфейса

---
+++ Infrastructure/Services/ResourceService.cs
@@ -0,0 +1,33 @@
+﻿using Application.DTO;
+using Domain.Interfaces;
+
+namespace Infrastructure.Services
+{
+    public class ResourceService : IResourceService
+    {
+        public Task<ResourceDto> AddResourceAsync(ResourceDto resourceDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteResourceAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResourceDto>> GetAllResourcesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResourceDto> GetResourceByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResourceDto> UpdateResourceByAsync(ResourceDto resourceDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
